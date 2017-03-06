080dbcce3bd4b379255ac13ca1b81dbf60426f66
Christoph <mussichhaben@kennstenicht.de>
2017-03-06 23:25:59 +0100

    Chore(Firebase): add dotenv and firebase

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,21 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      databaseURL: process.env.databaseURL,
+      storageBucket: process.env.storageBucke,
+      messagingSenderId: process.env.messagingSenderId
+    },
+
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com 'https://*.firebaseio.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
