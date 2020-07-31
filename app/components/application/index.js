862c95123ce744eb4993ead763990cd8229bbc6c
Christoph <christoph@ag-prop.com>
2020-07-31 08:56:53 +0200

    Feat(EmberAnimated): Install addon and setup debugging

--- app/components/application/index.js
+++ app/components/application/index.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import ENV from 'quizzen/config/environment';
 
 export default class ApplicationComponent extends Component {
   // Services
@@ -14,6 +15,11 @@ export default class ApplicationComponent extends Component {
   // Defaults
   @tracked showCookieNotice = false;
 
+
+  // Getter and Setter
+  get isDevelopment() {
+    return ENV.environment === 'development';
+  }
 
   // Hooks
   constructor() {
