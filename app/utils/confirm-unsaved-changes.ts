9658a8a9f9ce4378cfbbaee3076ce161946b752c
Christoph <christoph@ag-prop.com>
2020-11-27 11:22:15 +0100

    Clean(ConfirmUnsavedChanges): Remove log

--- app/utils/confirm-unsaved-changes.ts
+++ app/utils/confirm-unsaved-changes.ts
@@ -1,9 +1,8 @@
 import Transition from '@ember/routing/-private/transition';
 
 export default function confirmUnsavedChanges(transition: Transition, controller: any) {
-  console.log(controller.changeset);
   if (controller.changeset.isDirty) {
-    if (!confirm('You have unsaved changes.  Are you sure you want to leave this page?')){
+    if (!confirm('You have unsaved changes. Are you sure you want to leave this page?')){
       transition.abort();
     } else {
       controller.changeset.rollback();
