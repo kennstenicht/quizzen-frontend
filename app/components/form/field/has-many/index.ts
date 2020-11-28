511e276fa4c4685cae55c40ce7c5588dff804b36
Christoph <christoph@ag-prop.com>
2020-11-28 11:31:25 +0100

    Fix(FormFieldHasMany): Fix addRecord action

--- app/components/form/field/has-many/index.ts
+++ app/components/form/field/has-many/index.ts
@@ -2,20 +2,40 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Store from '@ember-data/store';
+import { BufferedChangeset } from 'ember-changeset/types';
 
 interface Args {
-  records: any[]
+  records: any[],
+  property: string,
+  changeset: BufferedChangeset
 }
 
 export default class FormFieldHasManyComponent extends Component<Args> {
+  // Services
   @service store!: Store;
 
+
+  // Getter and setter
+  get relationName() {
+    let property = this.args.property;
+    let model = this.args.changeset.data;
+    // @ts-ignore
+    let relationships = model.constructor.relationshipsByName;
+    let relationship = relationships.get(property);
+
+    return relationship.type;
+  }
+
+
+
+  // Actions
   @action
   addRecord() {
-    let newRecord = this.store.createRecord('answer', {
-      label: 'test'
-    });
+    let changeset = this.args.changeset;
+    let property = this.args.property;
+    let newRecord = this.store.createRecord(this.relationName);
 
-    this.args.records.pushObject(newRecord);
+    changeset.set(property, [...changeset.get(property).toArray(), newRecord]);
+
   }
 }
