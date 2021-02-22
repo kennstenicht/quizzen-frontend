38552db7b7e0c5fa7dfa967e71990f41ba36f07a
Christoph <christoph@ag-prop.com>
2021-02-22 21:48:40 +0100

    Feat(Serializer): Switch relation and attribute keys to underscore

--- app/serializers/application.ts
+++ app/serializers/application.ts
@@ -1,4 +1,12 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
+import { underscore } from '@ember/string';
 
 export default class ApplicationSerializer extends JSONAPISerializer {
+  keyForRelationship(key: string) {
+    return underscore(key);
+  }
+
+  keyForAttribute(attr: string) {
+    return underscore(attr);
+  }
 }
