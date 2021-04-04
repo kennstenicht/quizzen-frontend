30772b3a5d546ca130fd5b1b4c1b9f5a8e6c292f
Christoph <christoph@ag-prop.com>
2021-04-04 18:16:59 +0200

    Fix(UiAccordionItem): Clean imports

--- app/components/ui/accordion/item/index.ts
+++ app/components/ui/accordion/item/index.ts
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 import Accordion from 'quizzen/services/accordion';
 
 interface Args {
