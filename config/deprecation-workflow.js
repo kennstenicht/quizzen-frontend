803d4a67fe5484958184fc906ae9002f5b4eb483
Christoph <christoph@ag-prop.com>
2021-04-29 20:56:28 +0200

    Chore(Ember): Update to 3.26.1

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,5 +2,6 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+    { handler: 'silence', matchId: 'implicit-injections' }
   ]
 };
